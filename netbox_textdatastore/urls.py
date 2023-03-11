8dc63538412fd9f0766686a114a7cdf3d0259162
Michael Salathe <msalathe@clairglobal.com>
2023-03-11 15:34:21 +0100

    fixed url reference

--- netbox_textdatastore/urls.py
+++ netbox_textdatastore/urls.py
@@ -9,12 +9,12 @@ from .models import TextDataObject
 urlpatterns = (
 
     # modules
-    path('data/', views.TextDataObjectListView.as_view(), name='TextDataObject_list'),
-    path('data/<int:pk>/', views.TextDataObjectView.as_view(), name='TextDataObject'),
-    path('data/add/', views.TextDataObjectEditView.as_view(), name='TextDataObject_add'),
-    path('data/<int:pk>/edit/', views.TextDataObjectEditView.as_view(), name='TextDataObject_edit'),
-    path('data/<int:pk>/delete/', views.TextDataObjectDeleteView.as_view(), name='TextDataObject_delete'),
-    path('data/<int:pk>/changelog/', ObjectChangeLogView.as_view(), name='TextDataObject_changelog', kwargs={
+    path('data/', views.TextDataObjectListView.as_view(), name='textdataobject_list'),
+    path('data/<int:pk>/', views.TextDataObjectView.as_view(), name='textdataobject'),
+    path('data/add/', views.TextDataObjectEditView.as_view(), name='textdataobject_add'),
+    path('data/<int:pk>/edit/', views.TextDataObjectEditView.as_view(), name='textdataobject_edit'),
+    path('data/<int:pk>/delete/', views.TextDataObjectDeleteView.as_view(), name='textdataobject_delete'),
+    path('data/<int:pk>/changelog/', ObjectChangeLogView.as_view(), name='textdataobject_changelog', kwargs={
         'model': TextDataObject
     }),
 
