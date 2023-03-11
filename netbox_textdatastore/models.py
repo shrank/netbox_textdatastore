0708dd89138d08f4656d0010a2c44486952b4652
Michael Salathe <msalathe@clairglobal.com>
2023-03-11 16:13:23 +0100

    wrong path in get_absolute_url

--- netbox_textdatastore/models.py
+++ netbox_textdatastore/models.py
@@ -37,4 +37,4 @@ class TextDataObject(NetBoxModel):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('plugins:netbox_textdatastore:TextDataObject', args=[self.pk])
+        return reverse('plugins:netbox_textdatastore:textdataobject', args=[self.pk])
