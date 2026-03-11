37976d3896acb805c04e606b44e83398d3306528
sloweclair <slowe@clairglobal.com>
2026-03-10 23:47:10 -0400

    Proper models

--- netbox_textdatastore/template_content.py
+++ netbox_textdatastore/template_content.py
@@ -1,15 +1,11 @@
 from netbox.plugins import PluginTemplateExtension
 from . import models, tables
-from dcim.models import Device
 
 
 class DeviceRawData(PluginTemplateExtension):
-    model = 'dcim.device'
+    models = ['dcim.device']
 
     def right_page(self):
-        if not isinstance(self.context['object'], Device):
-            return ''
-            
         return self.render('netbox_textdatastore/device_incl.html', extra_context={
             'table': tables.TextDataObjectTable(models.TextDataObject.objects.filter(device=self.context['object'])),
         })
