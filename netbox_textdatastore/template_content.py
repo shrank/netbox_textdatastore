e088d2e54dd5d9be932f8fc47c7ce976aab54b80
sloweclair <slowe@clairglobal.com>
2026-03-10 22:08:36 -0400

    see if this works

--- netbox_textdatastore/template_content.py
+++ netbox_textdatastore/template_content.py
@@ -1,11 +1,15 @@
 from netbox.plugins import PluginTemplateExtension
 from . import models, tables
+from dcim.models import Device
 
 
 class DeviceRawData(PluginTemplateExtension):
     model = 'dcim.device'
 
     def right_page(self):
+        if not isinstance(self.context['object'], Device):
+            return ''
+            
         return self.render('netbox_textdatastore/device_incl.html', extra_context={
             'table': tables.TextDataObjectTable(models.TextDataObject.objects.filter(device=self.context['object'])),
         })
